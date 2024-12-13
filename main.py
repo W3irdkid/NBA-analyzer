1741a3a03ac783c64867a3a768d9eb355eda71ce
unknown <comeinpeace8@protonmail.com>
2024-12-13 14:38:14 +0100

    Improve  user input

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from nba_api.live.nba.endpoints import scoreboard
 
+
 # Get today's gamesgit 
 scores = scoreboard.ScoreBoard()
-def get_gameId():
-    games = scores.games.get_dict()  # Pobieramy słownik gier
+
+def get_game_id():
+    games = scores.games.get_dict()
     for game in games:
         game_id = game['gameId']
-        home_team = game['homeTeam']['teamName']  # Poprawiony dostęp do teamName
-        away_team = game['awayTeam']['teamName']  # Poprawiony dostęp do teamName
+        home_team = game['homeTeam']['teamName']
+        away_team = game['awayTeam']['teamName']
         
         print(f'Game ID: {game_id}')
         print(f'Home Team: {home_team}')
         print(f'Away Team: {away_team}')
 
-user_input = input("Do u want to check today games? (Y/N): ")
-if user_input == "Y":
-    get_gameId()
-elif user_input == "N": 
+
+user_input = input("Do u want to check today games? (y/n): ").strip().lower()
+if user_input == "y":
+    get_game_id()
+elif user_input == "n":
     print("Bye!")
     exit()
 else:
     exit()
-
